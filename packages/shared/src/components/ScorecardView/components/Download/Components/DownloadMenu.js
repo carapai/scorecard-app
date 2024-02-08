1c82acce0d118088969e8ca0afdbfc15d092bfbf
Charles Olupot <olupotcharles@gmail.com>
2024-02-08 12:27:49 +0300

    Added alert after sending data

--- packages/shared/src/components/ScorecardView/components/Download/Components/DownloadMenu.js
+++ packages/shared/src/components/ScorecardView/components/Download/Components/DownloadMenu.js
@@ -1,4 +1,5 @@
 import { FlyoutMenu, MenuItem } from "@dhis2/ui";
+import { useAlert } from "@dhis2/app-runtime";
 import PropTypes from "prop-types";
 import React from "react";
 import { useRecoilState } from "recoil";
@@ -10,19 +11,33 @@ export default function DownloadMenu({ onClose, onDownload }) {
     const [organisationUnit] = useRecoilState(
         ScorecardViewState("orgUnitSelection")
     );
+    const { show } = useAlert(
+        ({ message }) => message,
+        ({ type }) => ({ ...type, duration: 3000 })
+    );
     const [period] = useRecoilState(ScorecardViewState("periodSelection"));
     const postToAlma = async ({ scorecard }) => {
         if (organisationUnit.orgUnits.length > 0 && period.periods.length > 0) {
             try {
-                axios.post("https://services.dhis2.hispuganda.org/api/alma", {
-                    dx: `IN_GROUP-SWDeaw0RUyR`,
-                    pe: period.periods[0].id,
-                    scorecard,
-                    ou: organisationUnit.orgUnits[0].id,
-                    level: organisationUnit.orgUnits[0].level,
+                await axios.post(
+                    "https://services.dhis2.hispuganda.org/api/alma",
+                    {
+                        dx: `IN_GROUP-SWDeaw0RUyR`,
+                        pe: period.periods[0].id,
+                        scorecard,
+                        ou: organisationUnit.orgUnits[0].id,
+                        level: organisationUnit.orgUnits[0].level,
+                    }
+                );
+                show({
+                    message: "Data upload to alma is running in the background",
+                    type: { critical: false },
                 });
             } catch (error) {
-                console.log(error.message);
+                show({
+                    message: error.message ?? e.toString(),
+                    type: { critical: true },
+                });
             }
         }
     };
